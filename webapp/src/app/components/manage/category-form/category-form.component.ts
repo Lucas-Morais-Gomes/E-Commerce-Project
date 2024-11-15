72e9685d979cd000c65d707a854bac83431a3d0d
Lucas Morais Gomes <lucasmorais947@gmail.com>
2024-11-15 00:50:51 -0300

    list of categories and create new category page

---
+++ webapp/src/app/components/manage/category-form/category-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-category-form',
+  standalone: true,
+  imports: [FormsModule, MatInputModule, MatButtonModule],
+  templateUrl: './category-form.component.html',
+  styleUrl: './category-form.component.scss',
+})
+export class CategoryFormComponent {}
